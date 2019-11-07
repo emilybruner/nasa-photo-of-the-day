51d1de47ef54c845df91352516706641155f8b7d
Emily <bruner.emilym@gmail.com>
2019-11-07 16:39:56 -0500

    created basic card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import NasaInfo from "./components/NasaInfo";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/NavBar.js';
 // import { Button } from 'reactstrap';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <NasaInfo />
     </div>
   );
--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,15 +1,46 @@
 import React from "react";
+import styled from "styled-components";
 
+const CardContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 5%;
+padding: 2%;
+`
+const PhotoCard = styled.div`
+background: black;
+color: white;
+display: flex;
+flex-direction: column;
+align-items: center;
+max-width: 1000px;
+`;
+
+const CardH1 = styled.h1`
+font-size: 2rem;
+color: orange;
+`;
+
+const CardP = styled.p`
+max-width: 600px;
+font-size: 1.2rem;
+`;
+const CardImg = styled.img`
+
+`
 
 const NasaCard = props => {
     return (
+        <CardContainer>
+            <PhotoCard>
 
-        <div className="nasa-card">
-            <h1>{props.title}</h1>
-            <h2>{props.date}</h2>
-            <img className="nasa-image" alt="nasa pic of the day" src={props.imgUrl} />
-            <p>{props.explanation}</p>
-        </div>
+                <CardImg className="nasa-image" alt="nasa pic of the day" src={props.imgUrl} />
+                <h2>{props.date}</h2>
+                <CardH1>{props.title}</CardH1>
+                <CardP>{props.explanation}</CardP>
+            </PhotoCard>
+        </CardContainer>
     );
 };
 
